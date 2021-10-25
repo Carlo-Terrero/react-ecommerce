8c903f8c455e893f3e9a8d088ca5f7943523a7ef
Carlo-Terrero <carlosjose2111@gmail.com>
2021-10-25 12:39:11 +0100

    Componente de Login terminado

--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -3,11 +3,31 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 
 import {FormInput, FormButton} from '../formFields';
+import Details from '../details';
+
+import history from '../../history';
 
 class SignInFrom extends Component {
 
     render() {
         const { className, handleSubmit } = this.props;
+        const links = [
+            {
+                _id: 0,
+                title: 'Not registered? Create account here',
+                onClick: () => history.push('/signup')
+            },
+            {
+                _id: 1,
+                title: 'Forgot account email?',
+                onClick: () => console.log('forgot email')
+            },
+            {
+                _id: 2,
+                title: 'Forgot password?',
+                onClick: () => console.log('forgot password')
+            }
+        ]
         return (
             <form onSubmit={handleSubmit} className={`${className} sign-in-form`}>
                 <Field className='sign-in-form__email'
@@ -24,12 +44,15 @@ class SignInFrom extends Component {
                 name='password'
                 component={FormInput}/>
 
-                 <Field className='sign-in-form__login'
+                <div className='sign-in-form__line'></div>
+                
+                <Field className='sign-in-form__login'
                 onClick={() => console.log('tryna submit')}
                 type='submit'
                 title='Login'
                 name='login'
                 component={FormButton}/>
+                <Details className='sign-in-form__details' title='QuickLinks' links={links}/>
             </form>
         )
     }
