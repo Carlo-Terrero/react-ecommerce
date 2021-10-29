bd860d3703d8f29a91288c2f953c821fec59fc53
Carlo-Terrero <carlosjose2111@gmail.com>
2021-10-29 20:32:53 +0100

    Colocamos los porductos y hacemos el efecto de cambio pracido a cuando volteas una carta

---
+++ src/components/shop/shopSearchBar.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+import { reduxForm, Field } from 'redux-form';
+
+function FormSearchBar(props) {
+    return (
+        <div className='search-bar-grid'>
+            {/* Tambien de pueden poner los icono como si fuera html normal */}
+            <i className="fas fa-search search-bar-grid__icon"></i>
+            <input className={`${props.className} form-search-bar search-bar-grid__input`} {...props.input} type='text' placeholder={`${props.placeholder}`} />
+        </div>
+    )
+}
+
+{/* <i class="fas fa-search"></i> */}
+
+class ShopSearchBar extends Component {
+    render() {
+        const { className, handleSubmit } = this.props;
+        return (
+            <form onSubmit={handleSubmit} className={`${className} shop-search-bar`}>
+                <Field name='query' className='shop-search-bar__form-search-bar' placeholder='search' component={FormSearchBar} />
+            </form>
+        )
+    }    
+}
+
+ShopSearchBar = reduxForm({
+    form: 'ShopSearchBar'
+})(ShopSearchBar);
+
+export default ShopSearchBar; 
