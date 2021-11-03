4186999a2a4d69cdeec2c273fd427999516fece4
Carlo-Terrero <carlosjose2111@gmail.com>
2021-11-03 18:09:15 +0000

    carrito teminado y primeros pasos en verificar compra

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -4,8 +4,17 @@ import { connect } from 'react-redux';
 import * as actions from '../../actions';
 import ShopSearchBar from './shopSearchBar';
 import ShopProduct from './shopProduct';
+import ShopCart from './shopCart';
+import CartButton from './cartButton';
 
 class Shop extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            showCart: true
+        }
+    }
 
     componentDidMount() {
         const headerLinks = [
@@ -33,7 +42,17 @@ class Shop extends Component {
         this.props.filterProductsWithQuery(fields);
     }
 
+    
+    handleAddToCart = () => {
+        if(document.getElementById('shop-cart').classList.contains('cart-hidden')) {
+            document.getElementById('shop-cart').classList.remove('cart-hidden');
+        } else {
+            document.getElementById('shop-cart').classList.add('cart-hidden');
+        }
+    }
+
     render() {
+        //return <ShopCart className='shop__cart'/>
 
         return (
             <div className='shop'>
@@ -47,7 +66,12 @@ class Shop extends Component {
                         })
                     }
                 </div>
-                {/* shop cart button */}
+                {
+                    this.state.showCart ? <ShopCart className='shop__cart'/> : ''
+                }
+
+                <CartButton onClick={this.handleAddToCart} className='shop__cart-button' icon='fas fa-cart-plus'/>
+               {/*  <ShopCart/> */}
             </div>
         )
     }
